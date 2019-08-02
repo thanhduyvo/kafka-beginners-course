04c74e4e19fac29ce970bbbb7aa9b142f47b0604
thanhduyvo <@Dminct2b>
2019-08-02 12:07:14 -0500

    Update Bank Balance App

---
+++ kafka-streams-starter/src/main/java/kafka/streams/BankBalanceExactlyOnceApp.java
@@ -0,0 +1,96 @@
+package kafka.streams;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.time.Instant;
+import java.util.Properties;
+
+public class BankBalanceExactlyOnceApp {
+
+    public static void main(String[] args) {
+
+        // create properties
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "bank-balance-application");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // disable the cache to demonstrate all the "steps" involved in the transformation
+        properties.setProperty(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+
+        // exactly one processing
+        properties.setProperty(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+        // json Serde
+        final Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+        final Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+        final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+
+        // create a topology
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        // create the topic of bank transactions
+        KStream<String, JsonNode> bankTransactions = streamsBuilder.stream("bank-transactions",
+                Consumed.with(Serdes.String(), jsonSerde));
+
+        // create the initial json object for balances
+        ObjectNode initialBalance = JsonNodeFactory.instance.objectNode();
+        initialBalance.put("count", 0);
+        initialBalance.put("balance", 0);
+        initialBalance.put("time", Instant.ofEpochMilli(0L).toString());
+
+        KTable<String, JsonNode> bankBalance = bankTransactions
+                .groupByKey(Serialized.with(Serdes.String(), jsonSerde))
+                .aggregate(
+                        () -> initialBalance,
+                        (key, transaction, balance) -> newBalance(transaction, balance),
+                        Materialized.<String, JsonNode, KeyValueStore<Bytes, byte[]>>as("bank-balance-agg")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(jsonSerde)
+                );
+
+        bankBalance.toStream().to("bank-balance-exactly-once", Produced.with(Serdes.String(), jsonSerde));
+
+        // build the topology
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+
+        // start our streams application
+        kafkaStreams.cleanUp(); // only do this in Dev, not in Prod
+        kafkaStreams.start();
+
+        // print the topology
+        System.out.println(streamsBuilder.toString());
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+    }
+
+    private static JsonNode newBalance(JsonNode transaction, JsonNode balance) {
+        // create a new balance json object
+        ObjectNode newBalance = JsonNodeFactory.instance.objectNode();
+        newBalance.put("count", balance.get("count").asInt() + 1);
+        newBalance.put("balance", balance.get("balance").asInt() + transaction.get("amount").asInt());
+
+        Long balanceEpoch = Instant.parse(balance.get("time").asText()).toEpochMilli();
+        Long transactionEpoch = Instant.parse(transaction.get("time").asText()).toEpochMilli();
+        Instant newBalanceInstant = Instant.ofEpochMilli(Math.max(balanceEpoch, transactionEpoch));
+        newBalance.put("time", newBalanceInstant.toString());
+
+        return newBalance;
+    }
+}
---
+++ kafka-streams-starter/src/main/java/kafka/streams/BankTransactionsProducer.java
@@ -0,0 +1,67 @@
+package kafka.streams;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Instant;
+import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BankTransactionsProducer {
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // producer acks
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
+
+        // leverage idempotent producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+
+        // create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        int i = 0;
+        while(true) {
+            System.out.println("Producing batch: " + i);
+            try {
+                producer.send(newRandomTransaction("john"));
+                Thread.sleep(100);
+                producer.send(newRandomTransaction("stephane"));
+                Thread.sleep(100);
+                producer.send(newRandomTransaction("alice"));
+                Thread.sleep(100);
+                i += 1;
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+
+        // close producer
+        producer.close();
+    }
+
+    private static ProducerRecord<String, String> newRandomTransaction(String name) {
+        // create an empty json {}
+        ObjectNode transaction = JsonNodeFactory.instance.objectNode();
+        Integer amount = ThreadLocalRandom.current().nextInt(0, 100);
+
+        // get the current time using Java 8
+        Instant now = Instant.now();
+        transaction.put("name", name);
+        transaction.put("amount", amount);
+        transaction.put("time", now.toString());
+        return new ProducerRecord<>("bank-transactions", name, transaction.toString());
+    }
+}
